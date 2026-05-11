8a7755250b1e06ca97abc922013ca6a03078d07b
Denis López <denuxs@gmail.com>
2026-05-11 14:10:13 -0600

    init vue with tailwind

---
+++ src/views/QuoterView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const quote = ref('')
+</script>
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-bold underline">This is a quoter page</h1>
+  </div>
+</template>
+
+<style></style>
